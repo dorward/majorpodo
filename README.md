7d1aeb3d90461e4cf77ca8566df23fb185b623bd
David Dorward <david@dorward.me.uk>
2019-06-15 10:47:15 +0100

    Bulk of the functionality

---
+++ README.md
@@ -0,0 +1,42 @@
+# Majorpodo
+> A podcast server that uses metadata from static audio files
+
+Given a directory, Majorpodo will serve the audio files it finds there over HTTP
+along with webpages and RSS feeds generated from the metadata inside those
+files.
+
+Use it to host your own podcast, or provide a means to get files [downloaded
+from BBC radio](https://github.com/get-iplayer/get_iplayer) into your podcast
+client for convenient personal use (Majorpodo supports password protection).
+
+## Installation
+
+1. Checkout the project from Github
+2. `npm install`
+
+## Configuration
+
+Majorpodo uses [config](https://www.npmjs.com/package/config) for configuration.
+You will find an example configuration file in `config/dev.yml`. You will
+probably want to create a `config/default.yml` (or `.json` etc).
+
+To run with the development configuration:
+
+    NODE_ENV=dev node index.js
+
+## Meta
+
+Your Name – [@dorward](https://twitter.com/dorward) – david@dorward.me.uk
+
+Distributed under the XYZ license. See ``LICENSE`` for more information.
+
+[https://github.com/dorward/](https://github.com/dorward/)
+
+## Contributing
+
+1. Fork it (<https://github.com/dorward/majorpodo/fork>)
+2. Create your feature branch (`git checkout -b feature/fooBar`)
+3. Commit your changes (`git commit -am 'Add some fooBar'`)
+4. Push to the branch (`git push origin feature/fooBar`)
+5. Create a new Pull Request
+
--- index.js
+++ index.js
@@ -1,21 +1,106 @@
-const path = "/home/downloaduser/get_iplayer/";
+const config = require("config");
 const podcast = require("podcast");
 const express = require("express");
 const morgan = require("morgan");
+const basicAuth = require("express-basic-auth");
 const serveStatic = require("serve-static");
-const { getAudioFiles, getMetaData, filterMetaDataFactory, groupByAlbum, removeEpisodesWithMissingMetaData } = require("./lib/audio_search.js");
+const nunjucks = require("nunjucks");
+const { getAudioFiles, getMetaDataFactory, filterMetaDataFactory, groupByAlbum, removeEpisodesWithMissingMetaData } = require("./lib/audio_search.js");
 
+const path = config.get("path");
+const http_path = config.get("httpRoot");
+const users = config.get("users").reduce( (accumulator, currentValue) => {
+    accumulator[currentValue.username] = currentValue.password;
+    return accumulator;
+}, {} );
+
+const getMetaData = getMetaDataFactory(path);
+const filterMetaData = filterMetaDataFactory(path);
+let showindex = updateShowIndex();
+setInterval(() => showindex = updateShowIndex(), 60000 * 90); // Refresh every 90 minutes
+nunjucks.configure("views", { autoescape: true });
 
 const app = express();
-app.use(morgan("tiny"));
-app.get("/rss", serveRSS);
+app.use(morgan("tiny")); // Request logger middleware
+if (users.length) {
+    app.use(basicAuth({
+        users,
+        challenge: true,
+        realm: "majorpodo",
+    }));
+}
+app.get("/", listShows);
+app.get("/:show", aboutShow);
+app.get("/:show/rss", aboutShowRSS);
 app.get("/json", serveDebugData);
-app.use("/static", serveStatic(path));
+app.use("/audio", serveStatic(path));
+app.use("/static", serveStatic("static"));
 
-app.listen(3000, () => process.stdout.write("podcast_get_iplayer now on port 3000\n"));
+app.listen(config.get("listen"), () => process.stdout.write("Majorpodo server now listening\n") );
+
+function listShows(req, res) {
+    try {
+        showindex.then(generateShowList);
+    } catch (error) {
+        console.log("Error trying to use showindex which should be a promise", { showindex, error });
+    }
+    
+    function generateShowList(data) {
+        var shows = Object.keys(data).sort();
+        res.send(nunjucks.render("index.html", { shows }));
+    }
+}
+
+function aboutShow(req, res) {
+    try {
+        showindex.then(generateShowInfo);
+    } catch (error) {
+        console.log("Error trying to use showindex which should be a promise", { showindex, error });
+    }
+
+    function generateShowInfo(data) {
+        let show = data[req.params.show];
+        if (!show) {
+            res.status(404).send("Sorry can't find that!");
+        } else {
+            res.send(nunjucks.render("show.html", { show, title: req.params.show }));
+        }
+
+    }
+}
+
+function aboutShowRSS(req, res) {
+    try {
+        showindex.then(generateShowRSS);
+    } catch (error) {
+        console.log("Error trying to use showindex which should be a promise", { showindex, error });
+    }
+
+    function generateShowRSS(data) {
+        let show = data[req.params.show];
+        if (!show) {
+            res.status(404).send("Sorry can't find that!");
+        } else {
+            let rss_data = filterMetaData(show);
+
+            const feed = new podcast({
+                title: req.params.show,
+                feedUrl: http_path + encodeURIComponent(req.params.show) + "/rss",
+                siteUrl: http_path + encodeURIComponent(req.params.show),
+                description: "BBC radio programs collected using get_iplayer",
+                imageUrl: rss_data[0].image
+            });
+            rss_data.forEach(item => feed.addItem(item));
+            const xml = feed.buildXml("    ");
+            res.set("Content-Type", "application/rss+xml");
+            res.send(xml);
+        }
+
+    }
+}
 
 function serveDebugData(req, res) {
-    getAudioFiles(path).then(getMetaData).then(removeEpisodesWithMissingMetaData).then(groupByAlbum).then(generateJSON);
+    showindex.then(generateJSON);
 
     function generateJSON(data) {
         // Images are expensive!
@@ -25,19 +110,12 @@ function serveDebugData(req, res) {
     }
 }
 
-function serveRSS(req, res) {
-    getAudioFiles(path).then(getMetaData).then(removeEpisodesWithMissingMetaData).then(filterMetaDataFactory(path)).then(generateRSS);
-
-    function generateRSS(data) {
-        const feed = new podcast({
-            title: "iPlayer on the pod",
-            feedUrl: "http://dorward.no-ip.org:3000/rss",
-            siteUrl: "http://dorward.no-ip.org:3000/",
-            description: "BBC radio programs collected using get_iplayer"
-        });
-        data.forEach(item => feed.addItem(item));
-        const xml = feed.buildXml("    ");
-        res.set("Content-Type", "application/rss+xml");
-        res.send(xml);
-    }
+async function updateShowIndex() {
+    let files, meta, filtered, grouped;
+    files = await getAudioFiles(path);
+    meta = await getMetaData(files);
+    meta = meta.filter(element => element !== null);
+    filtered = await removeEpisodesWithMissingMetaData(meta);
+    grouped = await groupByAlbum(filtered);
+    return grouped;
 }
