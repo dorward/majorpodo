aa0c2c065596cc128e649ba5a26c4c029c138cf1
David Dorward <david@dorward.me.uk>
2020-12-05 20:43:10 +0000

    Support video as well as audio
    commit 2a4e3eeca8adce8147df0a6022c9eefbe07c3cf3
    Author: David Dorward <david@dorward.me.uk>
    Date:   Sat Dec 5 20:18:59 2020 +0000
    
        Indicate TV shows
    
    commit 7d27ca2388a94d3a5156c3248d26dc3d6e7b13f5
    Author: David Dorward <david@dorward.me.uk>
    Date:   Sat Dec 5 19:51:21 2020 +0000
    
        Support video files
    
    commit d67ccaa8156fa6f1cea6d867a0af2135037d11a4
    Author: David Dorward <david@dorward.me.uk>
    Date:   Sat Dec 5 19:28:11 2020 +0000
    
        Refactoring, linting, and a placeholder for video support
    
    commit f70d4cf527ec2657903db39e6cca99ae3d4a2384
    Author: David Dorward <david@dorward.me.uk>
    Date:   Sat Dec 5 19:16:23 2020 +0000
    
        A bunch of refactoring
    
    commit 25d2feb95ffc1ef950069554e9569b5cab9b9bc8
    Author: David Dorward <david@dorward.me.uk>
    Date:   Sat Dec 5 18:43:56 2020 +0000
    
        Rename audio to media and start factoring pieces out

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,12 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "sourceType": "module",
-        ecmaVersion: 2017,
-    },
-    "rules": {
-        "indent": [
-            "error",
-            4
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "double"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+	env: {
+		browser: true,
+		commonjs: true,
+		es2021: true,
+	},
+	extends: ['eslint:recommended', 'prettier'],
+	parserOptions: {
+		ecmaVersion: 12,
+	},
+	rules: {},
 };
