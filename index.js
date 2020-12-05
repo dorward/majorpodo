296887e67540ca2abff5403f39f6be0a1a0890cc
David Dorward <david@dorward.me.uk>
2020-12-05 18:23:31 +0000

    New design
    commit b30665baf69d452728b76ff492b7805393433db5
    Author: David Dorward <david@dorward.me.uk>
    Date:   Sat Dec 5 16:06:37 2020 +0000
    
        Add series list page
    
    commit 780b92c9ac2a687b0c9fa6a50ed407ec63e35425
    Author: David Dorward <david@dorward.me.uk>
    Date:   Sat Dec 5 15:56:26 2020 +0000
    
        Build episode page
    
    commit 9cfadf2d91c6ef707d4e660f8bd37bd0e3ee3da2
    Author: David Dorward <david@dorward.me.uk>
    Date:   Sat Dec 5 15:08:38 2020 +0000
    
        Create a base template to extend the others from
    
    commit 56db2aa9b706064d47d525663957ee1148317748
    Author: David Dorward <david@dorward.me.uk>
    Date:   Sat Dec 5 14:52:56 2020 +0000
    
        Better semantics
    
    commit 5dfef14ada820e90bf47869fa55291e92ae45d17
    Merge: 2157a4d b73ca58
    Author: David Dorward <david@dorward.me.uk>
    Date:   Sat Dec 5 14:50:59 2020 +0000
    
        Merge branch 'master' of github.com:dorward/majorpodo into new_page_layout
    
    commit 2157a4d8956265cd676afa1884904e92d832ca47
    Author: David Dorward <david@dorward.me.uk>
    Date:   Sat Dec 5 14:47:06 2020 +0000
    
        Design title page
    
    commit c49c72abe6b59c808a87d54010158dda21c7dfd3
    Author: David Dorward <david@dorward.me.uk>
    Date:   Sat Dec 5 14:46:42 2020 +0000
    
        Squashed commit of the following:
    
        commit b73ca5885ea6ab8904f88bff09647a73ec77b2a3
        Author: David Dorward <david@dorward.me.uk>
        Date:   Sat Dec 5 11:26:17 2020 +0000
    
            Cache metadata in local files for performance
    
    commit 3d1b697a1ad7ff190097f3664d1bdf293caf08f7
    Author: David Dorward <david@dorward.me.uk>
    Date:   Sat Dec 5 09:26:09 2020 +0000
    
        Make a start at a new page layout

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const serveStatic = require('serve-static');
 const nunjucks = require('nunjucks');
 const { httplogger, logger } = require('./lib/log');
 // eslint-disable-next-line no-unused-vars
-const { listShows, aboutShow, aboutShowRSS, serveDebugData } = require('./lib/routes');
+const { listShows, aboutShow, aboutShowRSS, serveDebugData, titleHomepage, episodePage } = require('./lib/routes');
 const path = config.get('path');
 const imagePath = config.get('imagePath');
 const auth = require('./lib/auth');
@@ -26,5 +26,7 @@ app.use('/static', serveStatic('static'));
 app.use('/images', serveStatic(imagePath));
 app.get('/:show', aboutShow);
 app.get('/:show/rss', aboutShowRSS);
+app.get('/title/:title', titleHomepage);
+app.get('/title/:title/episode/:episode', episodePage);
 
 app.listen(config.get('listen'), () => logger.log({ level: 'info', message: 'Majorpodo server now listening' }));
